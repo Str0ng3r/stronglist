b053e505f07c0cf7f7cf7ef448df7e69daca3ade
Maks <116167012+Str0ng3r@users.noreply.github.com>
2023-11-08 22:24:36 +0100

    UPDATE

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+
+
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Programm</li>
+                    <li>Profile</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
